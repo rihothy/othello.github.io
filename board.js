af43258d4dd4ffba849141fcbff77d19062dbac6
rihothy <1509269757@qq.com>
2024-05-08 00:17:10 +0800

    update

---
+++ board.js
@@ -0,0 +1,260 @@
+function sleep(ms) {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve();
+        }, ms);
+    });
+}
+
+async function playFlip(board, chess, type) {
+    let [white, black] = board.count();
+
+    if (chess.className !== '') {
+        chess.style.animation = 'disappear 0.15s ease-in-out';
+        await sleep(150);
+    }
+
+    document.getElementById('white-score').innerHTML = `${white}`;
+    document.getElementById('black-score').innerHTML = `${black}`;
+
+    chess.className = type + '-chess';
+    chess.style.animation = 'appear 0.2s ease-in-out';
+    await sleep(150);
+}
+
+let dir = [[-1, 0], [-1, 1], [0, 1], [1, 1], [1, 0], [1, -1], [0, -1], [-1, -1]];
+let weight = [
+    [90, -60, 10, 10, 10, 10, -60, 90],
+    [-60, -80, 5, 5, 5, 5, -80, -60],
+    [10, 5, 1, 1, 1, 1, 5, 10],
+    [10, 5, 1, 1, 1, 1, 5, 10],
+    [10, 5, 1, 1, 1, 1, 5, 10],
+    [10, 5, 1, 1, 1, 1, 5, 10],
+    [-60, -80, 5, 5, 5, 5, -80, -60],
+    [90, -60, 10, 10, 10, 10, -60, 90]
+];
+
+{
+    let sum = 0;
+
+    for (let line of weight) {
+        for (let val of line) {
+            sum += val;
+        }
+    }
+
+    for (let i = 0; i < 8; ++i) {
+        for (let j = 0; j < 8; ++j) {
+            weight[i][j] /= sum;
+        }
+    }
+}
+
+class Board {
+    constructor(chesses) {
+        this.data = [[0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 1, 2, 0, 0, 0], [0, 0, 0, 2, 1, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0]];
+        this.chesses = chesses;
+    }
+
+    copy(board) {
+        for (let i = 0; i < 8; ++i) {
+            for (let j = 0; j < 8; ++j) {
+                this.data[i][j] = board.data[i][j];
+            }
+        }
+    }
+
+    getType(i, j) {
+        if (this.data[i][j] == 1) {
+            return 'white';
+        } else if (this.data[i][j] == 2) {
+            return 'black';
+        } else {
+            return 'empty';
+        }
+    }
+
+    getNext(i, j, cur_type) {
+        let next = [0, 0, 0, 0, 0, 0, 0, 0], found = false;
+
+        if (this.getType(i, j) == 'empty') {
+            for (let k = 0; k < 8; ++k) {
+                for (let ti = i + dir[k][0], tj = j + dir[k][1], first = true; ti >= 0 && ti < 8 && tj >= 0 && tj < 8; ti += dir[k][0], tj += dir[k][1], first = false) {
+                    let type = this.getType(ti, tj);
+
+                    if (type == 'empty') {
+                        break;
+                    } else if (type == cur_type) {
+                        if (!first) {
+                            next[k] = 1;
+                            found = true;
+                        }
+
+                        break;
+                    }
+                }
+            }
+        }
+
+        return found ? next : undefined;
+    }
+
+    hasNext(cur_type) {
+        for (let i = 0; i < 8; ++i) {
+            for (let j = 0; j < 8; ++j) {
+                if (this.getNext(i, j, cur_type) !== undefined) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    count() {
+        let white = 0, black = 0;
+
+        for (let i = 0; i < 8; ++i) {
+            for (let j = 0; j < 8; ++j) {
+                let type = this.getType(i, j);
+
+                if (type == 'white') {
+                    ++white;
+                } else if (type == 'black') {
+                    ++black;
+                }
+            }
+        }
+
+        return [white, black];
+    }
+
+    eval(cur_type) {
+        let [white, black] = this.count();
+        let val = 0;
+        let cnt = 0;
+
+        for (let i = 0; i < 8; ++i) {
+            for (let j = 0; j < 8; ++j) {
+                let type = this.getType(i, j);
+
+                if (type == cur_type) {
+                    ++cnt;
+                    val += weight[i][j];
+                }
+            }
+        }
+
+        val /= cnt;
+        val *= (white + black) / 4;
+
+        if (cur_type == 'white') {
+            val += white - black;
+        } else {
+            val += black - white;
+        }
+
+        return val;
+    }
+
+    async move(i, j, next, cur_type, display) {
+        this.data[i][j] = cur_type == 'white' ? 1 : 2;
+
+        if (display) {
+            await playFlip(this, this.chesses[i * 8 + j], cur_type);
+        }
+
+        for (let k = 0; k < 8; ++k) {
+            if (next[k]) {
+                for (let ti = i + dir[k][0], tj = j + dir[k][1]; ti >= 0 && ti < 8 && tj >= 0 && tj < 8; ti += dir[k][0], tj += dir[k][1]) {
+                    let type = this.getType(ti, tj);
+
+                    if (type != cur_type) {
+                        this.data[ti][tj] = cur_type == 'white' ? 1 : 2;
+
+                        if (display) {
+                            await playFlip(this, this.chesses[ti * 8 + tj], cur_type);
+                        }
+                    } else {
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
+
+async function miniMax(board, ai_type) {
+    let cnt = 0;
+    let maxDepth = 7;
+    let hasDepth = false;
+    let player_type = ai_type == 'white' ? 'black' : 'white';
+    let dfs = async (board, depth, alpha, beta) => {
+        let val = depth % 2 ? 1000 : -1000;
+        let i = -1, j = -1;
+
+        ++cnt;
+
+        if (depth < maxDepth) {
+            for (let ti = 0; ti < 8 && alpha < beta; ++ti) {
+                for (let tj = 0; tj < 8 && alpha < beta; ++tj) {
+                    let next = board.getNext(ti, tj, depth % 2 ? player_type : ai_type);
+    
+                    if (next !== undefined) {
+                        let tboard = new Board(board.chesses);
+    
+                        tboard.copy(board);
+                        await tboard.move(ti, tj, next, depth % 2 ? player_type : ai_type, false);
+    
+                        let [tti, ttj, tval] = await dfs(tboard, depth + 1, alpha, beta);
+
+                        if (true) {
+                            if (depth % 2) {
+                                beta = Math.min(beta, tval);
+                            } else {
+                                alpha = Math.max(alpha, tval);
+                            }
+                        }
+    
+                        if (depth % 2 == 1 && tval < val) {
+                            val = tval, i = ti, j = tj;
+                        } else if (depth % 2 == 0 && tval > val) {
+                            val = tval, i = ti, j = tj;
+                        } else if (tval == val && Math.random() < 0.5) {
+                            i = ti, j = tj;
+                        }
+                    }
+                }
+            }
+        } else {
+            hasDepth = true;
+        }
+
+        if (i == -1) {
+            val = board.eval(ai_type);
+        }
+
+        // console.log('iter', depth, val);
+
+        return [i, j, val];
+    };
+
+    while (true) {
+        cnt = 0;
+        hasDepth = false;
+
+        let [i, j, val] = await dfs(board, 0, -1000, 1000);
+
+        // console.log('--------------');
+
+        // return [i, j];
+
+        if (cnt < 100000 && hasDepth) {
+            ++maxDepth;
+        } else {
+            console.log('cnt', cnt, maxDepth);
+
+            return [i, j];
+        }
+    }
+}
---
+++ index.js
@@ -0,0 +1,133 @@
+let cur_type = 'white';
+let cur_role = 'player';
+let board = undefined;
+let playing = false;
+let chesses = [];
+
+function checkResult() {
+    let notCanMove = !board.hasNext('white') && !board.hasNext('black');
+    let [white, black] = board.count();
+
+    if (white + black == 64 || !white || !black || notCanMove) {
+        document.body.firstElementChild.style.filter = 'blur(2px)';
+        document.getElementsByClassName('mask-end')[0].style.display = 'block';
+
+        if (white == black) {
+            document.getElementById('end-text').innerHTML = 'Draw!';
+        } else if (white > black && (cur_role == 'player' && cur_type == 'white' || cur_role == 'AI' && cur_type == 'black')) {
+            document.getElementById('end-text').innerHTML = 'Player Win!';
+        } else if (white < black && (cur_role == 'player' && cur_type == 'black' || cur_role == 'AI' && cur_type == 'white')) {
+            document.getElementById('end-text').innerHTML = 'Player Win!';
+        } else {
+            document.getElementById('end-text').innerHTML = 'AI Win!';
+        }
+
+        return true;
+    }
+
+    return false;
+}
+
+function flip() {
+    cur_type = cur_type == 'white' ? 'black' : 'white';
+    cur_role = cur_role == 'player' ? 'AI' : 'player';
+}
+
+async function playerPlay(i, j) {
+    if (playing) {
+        let next = board.getNext(i, j, cur_type);
+
+        if (next !== undefined) {
+            await board.move(i, j, next, cur_type, true);
+            flip();
+            playing = false;
+
+            if (checkResult()) {
+                return;
+            }
+
+            if (!board.hasNext(cur_type)) {
+                playing = true;
+                flip();
+                return;
+            }
+
+            document.getElementById('role-text').innerHTML = `AI Turn(${cur_type == 'white' ? 'White' : 'Black'})`;
+            setTimeout(() => {
+                AIPlay();
+            }, 100);
+        }
+    }
+}
+
+async function AIPlay() {
+    let [i, j] = await miniMax(board, cur_type);
+    let next = board.getNext(i, j, cur_type);
+
+    if (next !== undefined) {
+        await board.move(i, j, next, cur_type, true);
+        flip();
+
+        if (checkResult()) {
+            return;
+        }
+
+        if (!board.hasNext(cur_type)) {
+            flip();
+            AIPlay();
+            return;
+        }
+
+        document.getElementById('role-text').innerHTML = `Player Turn(${cur_type == 'white' ? 'White' : 'Black'})`;
+        playing = true;
+    } else {
+        alert('Error!');
+    }
+}
+
+window.addEventListener('load', (ev) => {
+    let table = document.createElement('table');
+
+    for (let i = 0; i < 8; ++i) {
+        let tr = document.createElement('tr');
+
+        for (let j = 0; j < 8; ++j) {
+            let td = document.createElement('td');
+
+            td.innerHTML = '<div class="cell"><div></div></div>';
+            tr.appendChild(td);
+
+            let chess = td.firstElementChild.firstElementChild;
+
+            chesses.push(chess);
+
+            if (i == 3 && j == 3 || i == 4 && j == 4) {
+                chess.className = 'white-chess';
+            } else if (i == 3 && j == 4 || i == 4 && j == 3) {
+                chess.className = 'black-chess';
+            }
+
+            td.addEventListener('click', (ev) => {
+                playerPlay(i, j);
+            });
+        }
+
+        table.appendChild(tr);
+    }
+
+    board = new Board(chesses);
+    document.body.firstElementChild.firstElementChild.appendChild(table);
+
+    document.getElementById('play-game-btn').addEventListener('click', (ev) => {
+        document.body.firstElementChild.style.filter = 'blur(0px)';
+        document.body.firstElementChild.nextElementSibling.style.display = 'none';
+
+        if (document.getElementById('AI-first-check').checked) {
+            cur_role = 'AI';
+            document.getElementById('role-text').innerHTML = 'AI Turn(White)';
+            AIPlay();
+        } else {
+            playing = true;
+        }
+    });
+});
